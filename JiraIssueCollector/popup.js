b32b1dd1ff61b1ffa9c5a02932995e0c5120e6c1
Rubén Martínez <ruben.martinez@docomodigital.com>
2017-08-21 12:53:46 +0200

    before new-form changes

--- JiraIssueCollector/popup.js
+++
@@ -1,2 +0,0 @@
-chrome.windows.create({'url': chrome.extension.getURL("popup_window.html"), 'type': 'popup'}, function(window) {});
-
---
+++ extension/popup.js
@@ -0,0 +1,2 @@
+chrome.windows.create({'url': chrome.extension.getURL("popup_new_window.html"), 'type': 'popup'}, function(window) {});
+
